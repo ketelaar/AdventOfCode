45efe2e6935550ebfd55cc7717b67657beb1c60f
Kenny Ketelaar <kenny@ketelaar.tech>
2023-12-01 12:05:46 +0100

    Added solution for day 1, part 1

--- Rust/aoc2023/src/days/day01.rs
+++ Rust/aoc2023/src/days/day01.rs
@@ -1,3 +1,23 @@
 pub fn solve() {
-    println!("Hello from Day 1!")
+    let input = include_str!("../resources/day01.txt");
+    let answer_a = get_answer_a(input);
+
+    println!("Answer for the first part is: {answer_a}")
+}
+
+fn get_answer_a(input: &str) -> usize {
+    let lines = input.split_whitespace();
+    let mut numbers = vec![];
+
+    for line in lines {
+        let digits: Vec<char> = line.
+            chars()
+            .filter(|c| c.is_ascii_digit())
+            .collect();
+        let first_digit = digits.first().unwrap();
+        let last_digit = digits.last().unwrap();
+        let number: usize = format!("{first_digit}{last_digit}").parse().unwrap();
+        numbers.push(number)
+    }
+    numbers.iter().sum()
 }
