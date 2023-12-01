c3bc03b36258094f40f9fc126a58d52452d90447
Kenny Ketelaar <kenny@ketelaar.tech>
2023-12-01 13:39:03 +0100

    Reformatted day 1-1 a bit

--- Rust/aoc2023/src/days/day01.rs
+++ Rust/aoc2023/src/days/day01.rs
@@ -2,7 +2,7 @@ pub fn solve() {
     let input = include_str!("../resources/day01.txt");
     let answer_a = get_answer_a(input);
 
-    println!("Answer for the first part is: {answer_a}")
+    println!("Answer for day part 1 is: {answer_a}")
 }
 
 fn get_answer_a(input: &str) -> usize {
@@ -10,8 +10,8 @@ fn get_answer_a(input: &str) -> usize {
     let mut numbers = vec![];
 
     for line in lines {
-        let digits: Vec<char> = line.
-            chars()
+        let digits: Vec<char> = line
+            .chars()
             .filter(|c| c.is_ascii_digit())
             .collect();
         let first_digit = digits.first().unwrap();
