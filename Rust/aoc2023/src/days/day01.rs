3efd6c1152840d484125250d7d96f7ec4fe5d8e3
Kenny Ketelaar <kenny@ketelaar.tech>
2023-12-01 23:31:40 +0100

    Refactored answer for day 1 part 1

--- Rust/aoc2023/src/days/day01.rs
+++ Rust/aoc2023/src/days/day01.rs
@@ -10,17 +10,19 @@ pub fn solve() {
 }
 
 fn get_answer_a(input: &str) -> usize {
-    let lines = input.split_whitespace();
-    let mut numbers = vec![];
-
-    for line in lines {
-        let digits: Vec<char> = line.chars().filter(|c| c.is_ascii_digit()).collect();
-        let first_digit = digits.first().unwrap();
-        let last_digit = digits.last().unwrap();
-        let number: usize = format!("{first_digit}{last_digit}").parse().unwrap();
-        numbers.push(number)
-    }
-    numbers.iter().sum()
+    input
+        .lines()
+        .map(|line| {
+            let digits: Vec<char> = line
+                .chars()
+                .filter(|c| c.is_ascii_digit())
+                .collect();
+            let first_digit = digits.first().expect("should be a valid digit");
+            let last_digit = digits.last().expect("should be a valid digit");
+            let number: usize = format!("{first_digit}{last_digit}").parse().expect("should be a valid number");
+            number
+        })
+        .sum()
 }
 
 fn get_answer_b(input: &str) -> usize {
